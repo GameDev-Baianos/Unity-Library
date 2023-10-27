77e40fd4ad4ce3c3bd8190ac17083aac2e46db64
Arthur Luz <arthurluz04@hotmail.com>
2023-10-27 18:18:22 -0300

    FIxed collision problem and easy character controller
    Brackeys saving my ass again

--- 2D Top Down Pixel Art RPG/Assets/Characters/Player/PlayerController.cs
+++ 2D Top Down Pixel Art RPG/Assets/Characters/Player/PlayerController.cs
@@ -1,64 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 1;
-    public float collisionOffset = 0.05f;
-    public ContactFilter2D movementFilter;
+    public float moveSpeed = 1f;
+
+    public Rigidbody2D rb;
 
     Vector2 movementInput;
-    Rigidbody2D rb;
-    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        rb = GetComponent<Rigidbody2D>();
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        if(movementInput != Vector2.zero)
-        {
-            bool success = TryMove(movementInput);
-
-            if(!success)
-            {
-                success = TryMove(new Vector2(movementInput.x, 0));
-            }
-
-            if (!success)
-            {
-                success = TryMove(new Vector2(movementInput.y, 0));
-            }
-        }
-    }
-
-    private bool TryMove(Vector2 direction)
-    {
-        int count = rb.Cast(
-                        movementInput,
-                        movementFilter,
-                        castCollisions,
-                        moveSpeed * Time.fixedDeltaTime * collisionOffset
-                        );
-
-        if (count == 0)
-        { 
-            rb.MovePosition(rb.position + (movementInput * moveSpeed * Time.fixedDeltaTime));
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    void OnMove(InputValue movementValue)
-    {
-        movementInput = movementValue.Get<Vector2>();
-    }
+        rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+    }    
 }
