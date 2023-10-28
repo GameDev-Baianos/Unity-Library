0ab95c7247e9555d5ea641f51939a8c72abc9987
Arthur Luz <arthurluz04@hotmail.com>
2023-10-27 23:44:20 -0300

    Setting up some animations
    took me more than i expected...

--- 2D Top Down Pixel Art RPG/Assets/Characters/Player/PlayerController.cs
+++ 2D Top Down Pixel Art RPG/Assets/Characters/Player/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 1f;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movementInput;
 
@@ -12,6 +13,10 @@ public class PlayerController : MonoBehaviour
     {
         movementInput.x = Input.GetAxisRaw("Horizontal");
         movementInput.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movementInput.x);
+        animator.SetFloat("Vertical", movementInput.y);
+        animator.SetFloat("Speed", movementInput.sqrMagnitude);
     }
 
     void FixedUpdate()
