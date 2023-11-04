720a59b11266496b47b4ccf6d5cd481f01905b00
Arthur Luz <arthurluz04@hotmail.com>
2023-11-04 13:46:43 -0300

    A true script library
    basic topdown movement using transform.Translante and Vector3

--- Basic Third Person Character Controller/Assets/PlayerMovement.cs
+++ Basic Third Person Character Controller/Assets/PlayerMovement.cs
@@ -8,12 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float rotationSpeed;
 
     [SerializeField] float speed = 5;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
---
+++ Useful Scripts/TopDown movement/MoveScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    Vector3 moveDelta;
+
+    [SerializeField ]int moveSpeed;
+    float ySpeed = 0.75f;
+    float xSpeed = 1.0f;
+    
+    void Update()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput  = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(xInput * xSpeed, yInput * ySpeed, 0);
+
+        transform.Translate(moveDelta * moveSpeed * Time.deltaTime);
+    }
+}
