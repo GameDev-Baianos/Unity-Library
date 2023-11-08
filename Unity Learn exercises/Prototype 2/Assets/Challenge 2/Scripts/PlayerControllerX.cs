37300623a6a8c6ab475d6ed0e5c2e468053bf14b
Arthur Luz <arthurluz04@hotmail.com>
2023-11-08 06:41:00 -0300

    Challenge 2 Unity Learn
    Learned more about coroutines

---
+++ Unity Learn exercises/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float delay = 0.5f;
+    private bool isCooldown = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog in a delay interval
+        if (Input.GetKeyDown(KeyCode.Space) && !isCooldown)
+        {
+            StartCoroutine(instantiateDelay(delay));
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+
+    private IEnumerator instantiateDelay(float delay)
+    {
+        isCooldown = true;
+        yield return new WaitForSeconds(delay);
+        isCooldown = false;
+    }
+}
